7a8a4fe6c9c7bbdccc6ad59b3c323e3d51bd9a57
lrylander <linnearylander@gmail.com>
2019-01-06 11:22:39 -0800

    more stylistic homepage updates

--- js/homepage.js
+++ js/homepage.js
@@ -1,16 +1,18 @@
 $(function() {
    $(window).scroll(function () {
-      if ($(this).scrollTop() > $(document).height()*0.3) {
+      if ($(this).scrollTop() > $(document).height()*0.32) {
          $('body').addClass('changeColor')
       }
-      if ($(this).scrollTop() < $(document).height()*0.3) {
+      if ($(this).scrollTop() < $(document).height()*0.32) {
          $('body').removeClass('changeColor')
       }
-      if ($(this).scrollTop() > $(document).height()*0.703) {
+      if ($(this).scrollTop() > $(document).height()*0.74) {
          $('body').addClass('changeColor2')
       }
-      if ($(this).scrollTop() < $(document).height()*0.703) {
+      if ($(this).scrollTop() < $(document).height()*0.74) {
          $('body').removeClass('changeColor2')
       }
    });
 });
+
+
