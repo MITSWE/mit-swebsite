7881053f5ecfa9be835d3d5210d273e6ac5f26cd
lrylander <linnearylander@gmail.com>
2019-01-08 08:20:42 -0800

    Manually merging homepage-redesign into master

---
+++ js/homepage.js
@@ -0,0 +1,17 @@
+$(function() {
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > $(document).height()*0.33) {
+         $('body').addClass('changeColor')
+      }
+      if ($(this).scrollTop() < $(document).height()*0.33) {
+         $('body').removeClass('changeColor')
+      }
+      if ($(this).scrollTop() > $(document).height()*0.74) {
+         $('body').addClass('changeColor2')
+      }
+      if ($(this).scrollTop() < $(document).height()*0.74) {
+         $('body').removeClass('changeColor2')
+      }
+   });
+});
+
