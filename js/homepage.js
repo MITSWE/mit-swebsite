19ca9f5cb6c7fabaff5e9db3494f4f781c409377
lrylander <linnearylander@gmail.com>
2019-09-16 16:42:43 -0400

    update scroll spacing

--- js/homepage.js
+++ js/homepage.js
@@ -6,12 +6,11 @@ $(function() {
       if ($(this).scrollTop() < $(document).height()*0.31) {
          $('body').removeClass('changeColor')
       }
-      if ($(this).scrollTop() > $(document).height()*0.66) {
+      if ($(this).scrollTop() > $(document).height()*0.67) {
          $('body').addClass('changeColor2')
       }
-      if ($(this).scrollTop() < $(document).height()*0.66) {
+      if ($(this).scrollTop() < $(document).height()*0.67) {
          $('body').removeClass('changeColor2')
       }
    });
 });
-
